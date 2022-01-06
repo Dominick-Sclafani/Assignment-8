492a7b601e36f20ecfe9798e785c6d0e37c77ee2
Dominick Sclafani <dominick.sclafani@cix.csi.cuny.edu>
2022-01-06 17:39:14 -0500

    Finished and working assignment 3

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -16,11 +16,44 @@ console.log(document.querySelector("ol li.third"))
 
 // question 5 
 
-console.log(document.getElementById("container").innerHTML = "Hello!")
+var pDoc = document.createElement("p")
+pDoc.innerHTML = "Hello!"
+console.log(document.getElementById("container").append(pDoc))
 
 //question 6 
 
-console.log(document.getElementsByClassName("footer").classname = "main")
-                                                                                       
+console.log(document.getElementsByClassName("footer").classname = "main")                                                                                 
 //question 7
+
+console.log(document.querySelector(".footer").classList.remove("main"))
+
+
+
+
+//question 8 
+
+var li = document.createElement("li");
+console.log(li);
+//question 9 
+
+console.log(li.innerText = "four");
+
+
+//question 10
+console.log(document.getElementsByTagName("ul")[0].appendChild(li));
+
+
+
+//question 12
+let x=document.getElementById("container"); 
+let nodelist = x.getElementsByTagName("ul");
+for(let i=0; i < nodelist.length; i++){
+    nodelist[i].style.backgroundColor = "green";
+}
+
+
+//question 13
+
+console.log(document.getElementsByClassName("footer").remove);
+
 
