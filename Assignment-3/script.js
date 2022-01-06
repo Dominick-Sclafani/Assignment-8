6d0fd37a9968ae58954e59ee8de0f73111534fcb
Dominick Sclafani <dominick.sclafani@cix.csi.cuny.edu>
2022-01-06 14:53:27 -0500

    js first draft

---
+++ Assignment-3/script.js
@@ -0,0 +1,26 @@
+//question 1
+console.log(document.getElementById("container"))
+
+
+//question 2 
+
+console.log(document.querySelector("#container"))
+
+//question 3 
+
+console.log(document.getElementsByClassName("second"))
+
+//question 4
+
+console.log(document.querySelector("ol li.third"))
+
+// question 5 
+
+console.log(document.getElementById("container").innerHTML = "Hello!")
+
+//question 6 
+
+console.log(document.getElementsByClassName("footer").classname = "main")
+                                                                                       
+//question 7
+
