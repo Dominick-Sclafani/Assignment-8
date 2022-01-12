4181b12d303e062c622496a304246e2ff075beb8
Dominick Sclafani <dominick.sclafani@cix.csi.cuny.edu>
2022-01-12 10:55:55 -0500

    fixed the syntax related errors

--- Assignment-7/src/App.js
+++ Assignment-7/src/App.js
@@ -1,62 +1,53 @@
-import './App.css';
-import {useState, useEffect} from "react"
-import SearchField from './components/SearchField';
+import "./App.css";
+import { useState, useEffect } from "react";
+import SearchField from "./components/SearchField";
 
 function App() {
-
-  const baseURL = 'http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=${process.env.REACT_APP_API_KEY';
-  const [search, setSearch] = useState("");//set state for search text
-  const [gifs, setGifs] = useState ([]); //set state for gifs
-  const [loading, setLoading] = useState(false);//set state for loading
+  const baseURL =
+    "http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=${process.env.REACT_APP_API_KEY";
+  const [search, setSearch] = useState(""); //set state for search text
+  const [gifs, setGifs] = useState([]); //set state for gifs
+  const [loading, setLoading] = useState(false); //set state for loading
 
   let searchGif = () => {
-    if (search.length > 0){
+    if (search.length > 0) {
       setLoading(true);
-      fetch(baseURL+search)
-      .then((res)=>{
-        setLoading(true);
-        return res.json();
-      })
-      .then((result)=>{
-         setGifs(result.data.map((gif)=>{
-           return gif.images.fixed_height.url
-         }))
-      })
+      fetch(baseURL + search)
+        .then((res) => {
+          setLoading(true);
+          return res.json();
+        })
+        .then((result) => {
+          setGifs(
+            result.data.map((gif) => {
+              return gif.images.fixed_height.url;
+            })
+          );
+        })
 
-      .catch(()=>{
-        setLoading(false);
-        console.log("This did not work");
-      })
+        .catch(() => {
+          setLoading(false);
+          console.log("This did not work");
+        });
     }
-  }
-  
+  };
+
   return (
-    
-    <div className="header">
+    //<div className="header">
     <>
-
-    <p>Search GIFS</p>
-
-    value = {search}
-
-    onChange = {(e)=>setSearch(e.target.value)}
-
-    <button onClick = {searchGif}>
-      Search
-    </button>
-
-      gifs.map((gif)=>{
-      return (
-        <div className="box">
-          <img src={gif}/>
-        </div>
-     </div> )
-    });
+      <p>Search GIFS</p>
+      value = {search}
+      onChange = {(e) => setSearch(e.target.value)}
+      <button onClick={searchGif}>Search</button>
+      {gifs.map((gif) => {
+        return (
+          <div className="box">
+            <img src={gif} />
+          </div>
+        );
+      })}
     </>
-  );_
-
+  );
 }
-
-
 
 export default App;
